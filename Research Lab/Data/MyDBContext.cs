f73b1f0e4b516440acf9e17121c30e32d3c6dd4b
Sajidur Rahman <sajidur35-1831@diu.edu.bd>
2019-12-01 16:11:00 +0600

    Add project files.

---
+++ Research Lab/Data/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Research_Lab.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
